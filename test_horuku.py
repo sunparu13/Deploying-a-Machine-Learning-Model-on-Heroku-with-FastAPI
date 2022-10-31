e12ef95767a2bc12c80be4af9e2fc4170718ec94
CWS <46682029+ChenweiSun@users.noreply.github.com>
2022-10-31 08:52:26 +0100

    update test_heroku

--- test_horuku.py
+++ test_horuku.py
@@ -1,27 +1,23 @@
-import json
 import requests
 
-sample = {
-    "age": 53,
-    "workclass": "Private",
-    "fnlgt": 234721,
-    "education": "11th",
-    "education-num": 7,
-    "marital-status": "Married-civ-spouse",
-    "occupation": "Handlers-cleaners",
-    "relationship": "Husband",
-    "race": "Black",
-    "sex": "Male",
-    "capital-gain": 0,
-    "capital-loss": 0,
-    "hours-per-week": 40,
-    "native-country": "United-States"
-    }
+def test_data_post():
+    sample = {
+        "age": 53,
+        "workclass": "Private",
+        "fnlgt": 234721,
+        "education": "11th",
+        "education-num": 7,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Handlers-cleaners",
+        "relationship": "Husband",
+        "race": "Black",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+        }
 
-get_response = requests.get('https://udamlopsp3.herokuapp.com/')
-print(get_response.status_code)
-print(get_response.json())
-
-pred = requests.post('https://udamlopsp3.herokuapp.com/inference_main', json=sample)
-print(pred.status_code)
-print(pred.json())
+    pred = requests.post('https://udamlopsp3.herokuapp.com/inference_main', json=sample)
+    print(pred.status_code)
+    print(pred.json())
