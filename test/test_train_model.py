2742a83ca77b1796df97d5c9ce9fc1b3e0c3b3b2
CWS <46682029+ChenweiSun@users.noreply.github.com>
2022-11-01 09:50:16 +0100

    path correction

--- test/test_train_model.py
+++ test/test_train_model.py
@@ -2,8 +2,8 @@ import os
 import sys
 sys.path.append('/Users/sunparu/Documents/schulung/mldevops/Deploying-a-Machine-Learning-Model-on-Heroku-with-FastAPI/data')
 sys.path.append('/Users/sunparu/Documents/schulung/mldevops/Deploying-a-Machine-Learning-Model-on-Heroku-with-FastAPI/model')
-sys.path.append('/Users/sunparu/Documents/schulung/mldevops/Deploying-a-Machine-Learning-Model-on-Heroku-with-FastAPI/starter')
-from train_model import preprocessing, train
+
+from starter.train_model import preprocessing, train
 
 def test_preprocessing():
     X_train, _, _, _ = preprocessing("data/census_clean.csv")
@@ -12,7 +12,6 @@ def test_preprocessing():
     assert X_train.shape[1] == 12
     assert os.path.isfile("model/encoder.pkl")
     assert os.path.isfile("model/lb.pkl")
-
 
 def test_train(x,y):
     train(x, y)
