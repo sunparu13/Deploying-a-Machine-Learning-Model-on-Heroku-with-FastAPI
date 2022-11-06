0109a3534c32558c568b694c4d18276ea347a1dd
CWS <46682029+ChenweiSun@users.noreply.github.com>
2022-11-06 09:48:21 +0100

    change main

--- heroku_live.py
+++ heroku_live.py
@@ -6,16 +6,16 @@ lower_sample = {
     "workclass": "Private",
     "fnlgt": 234721,
     "education": "11th",
-    "education_num": 7,
-    "marital_status": "Married-civ-spouse",
+    "education-num": 7,
+    "marital-status": "Married-civ-spouse",
     "occupation": "Handlers-cleaners",
     "relationship": "Husband",
     "race": "Black",
     "sex": "Male",
-    "capital_gain": 0,
-    "capital_loss": 0,
-    "hours_per_week": 40,
-    "native_country": "United-States"
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
 }
 
 pred = requests.post('https://udamlopsp3.herokuapp.com/inference_main', json= lower_sample)
@@ -27,16 +27,16 @@ higher_sample = {
     "workclass": "Private",
     "fnlgt": 45781,
     "education": "Masters",
-    "education_num": 14,
-    "marital_status": "Never-married",
+    "education-num": 14,
+    "marital-status": "Never-married",
     "occupation": "Prof-specialty",
     "relationship": "Not-in-family",
     "race": "White",
     "sex": "Female",
-    "capital_gain": 14084,
-    "capital_loss": 0,
-    "hours_per_week": 50,
-    "native_country": "United-States"
+    "capital-gain": 14084,
+    "capital-loss": 0,
+    "hours-per-week": 50,
+    "native-country": "United-States"
 }
 
 pred = requests.post('https://udamlopsp3.herokuapp.com/inference_main', data=json.dumps(higher_sample))
